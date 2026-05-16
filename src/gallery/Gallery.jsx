df03dc1e6983454cda7f95c70b1fcf3f20f8a441
AMadayag <hiding303@gmail.com>
2026-05-16 15:00:23 +1000

    connected gallery to db

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import './Gallery.css'
-import { gallery_images } from './GalleryImages';
+import { gallery_images, getAllImages } from '../mongodb/GalleryImages';
 
 function Gallery({ onImageClick }) {
+  const [galleryImgs, setGalleryImgs] = useState([])
+
   const chunkArray = (arr, n) => // splits up gallery folder to chunks for rows
     Array.from({ length: n }, (_, i) => arr.filter((_, idx) => idx % n === i)
   );
 
-  const rows = chunkArray(gallery_images, 3);
+  useEffect(() => {
+    const fetchImgs = async () => {
+      const imgs = await getAllImages()
+      if (!imgs) {
+        setGalleryImgs(gallery_images.map(([_, mod]) => mod.default))
+      } else {
+        setGalleryImgs(imgs)
+      }
+    }
+    fetchImgs()
+  }, [])
+
+  // const rows = chunkArray(gallery_images, 3)
+  const rows = chunkArray(galleryImgs, 3)
 
   return (
     <div className='gallery-container'>
@@ -18,11 +34,11 @@ function Gallery({ onImageClick }) {
       </div>
       {rows.map((row, rowIndex) => (
         <div key={rowIndex} className='vertical-row'>
-          {row.map(([path, module]) => (
+          {row.map((url) => (
             <img
-              key={path}
-              src={module.default}
-              onClick={() => onImageClick(module.default)}
+              key={url}
+              src={url}
+              onClick={() => onImageClick(url)}
               alt='img'
               className='gallery-img'
             />
--- src/gallery/GalleryCloseup.jsx
+++ src/gallery/GalleryCloseup.jsx
@@ -2,58 +2,45 @@ import './GalleryCloseup.css'
 import React, { useEffect, useState } from 'react';
 import { IoCloseSharp } from "react-icons/io5";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
-import { gallery_images } from './GalleryImages';
+import { getAllImages } from '../mongodb/GalleryImages';
 
 function GalleryCloseup({ img_src, onClose }) {
+  const [galleryImages, setGalleryImages] = useState([]);
   let [displayImage, updateDisplayImage] = useState(img_src);
 
+  useEffect(() => { // image fetch
+    getAllImages().then(imgs => {
+      if (imgs) setGalleryImages(imgs);
+    });
+  }, []);
+
   useEffect(() => { // triggers onClose when esc key pressed
-    const handleEsc = (event) => {
+    const handleKeyPress = (event) => {
       if (event.key === 'Escape') {
         onClose()
+      } else if (event.key === 'ArrowLeft') {
+        imageLeft()
+      } else if (event.key === 'ArrowRight') {
+        imageRight()
       }
     };
 
-    window.addEventListener('keydown', handleEsc);
+    window.addEventListener('keydown', handleKeyPress)
 
     return () => {
-      window.removeEventListener('keydown', handleEsc);
+      window.removeEventListener('keydown', handleKeyPress)
     };
-  }, [onClose, displayImage]);
-
-  const listGalleryImages = () => {
-    const newArray = [];
-
-    for (const image of gallery_images) {
-      newArray.push('/WebcomicUNSW' + image[0]);
-    }
-
-    return newArray;
-  }
+  }, [onClose, displayImage, galleryImages])
 
   const imageLeft = () => {
-    const galleryImages = listGalleryImages();
-    let index = galleryImages.indexOf(displayImage);
-
-    if (index - 1 < 0) {
-      index = galleryImages.length - 1;
-    } else {
-      index = index - 1;
-    }
-
+    let index = galleryImages.indexOf(displayImage)
+    index = index - 1 < 0 ? galleryImages.length - 1 : index - 1;
     updateDisplayImage(galleryImages[index]);
   }
 
   const imageRight = () => {
-    const galleryImages = listGalleryImages();
     let index = galleryImages.indexOf(displayImage);
-
-    if (index + 1 > galleryImages.length - 1) {
-      index = 0;
-    } else {
-      index = index + 1;
-    }
-
+    index = index + 1 > galleryImages.length - 1 ? 0 : index + 1;
     updateDisplayImage(galleryImages[index]);
   }
 
