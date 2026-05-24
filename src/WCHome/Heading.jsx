ce836f75c9ff9fdeba09408e6909386d72bcef7f
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-05-24 22:15:40 +1000

    Began working on homepage and working on assets overlay

--- src/WCHome/Heading.jsx
+++ src/WCHome/Heading.jsx
@@ -4,7 +4,14 @@ function Heading() {
 
   return (
     <div className='header'>
-        <img src='src/WCHome/heading-svgs/comic-panel1.svg' alt='comic-panel-1'></img>
+        <div className='panel1'>
+            <img src='src/WCHome/heading-svgs/comic-panel1.svg' alt='comic-panel-1'></img>
+            <div className='banner'>
+                <img src='src/WCHome/heading-svgs/speech-bub-big.svg' alt='speech-bubble-big' className='speech-bubble-big'></img>
+                <h3 className='banner-text'>WE ARE WEBCOMIC UNSW</h3>
+            </div>
+        </div>
+        
         <img src='src/WCHome/heading-svgs/comic-panel2.svg' alt='comic-panel-2'></img>
     </div>
   )
