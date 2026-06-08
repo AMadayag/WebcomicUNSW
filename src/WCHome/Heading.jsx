7e562eb953fb4532b477a89205618b6d62f8fd19
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-06-08 17:40:59 +1000

    Added the main banner for the home page

--- src/WCHome/Heading.jsx
+++ src/WCHome/Heading.jsx
@@ -5,14 +5,17 @@ function Heading() {
   return (
     <div className='header'>
         <div className='panel1'>
-            <img src='src/WCHome/heading-svgs/comic-panel1.svg' alt='comic-panel-1'></img>
+            <img src='src/WCHome/heading-svgs/comic-panel1.svg' alt='comic-panel-1' className='comic-panel-1'></img>
             <div className='banner'>
                 <img src='src/WCHome/heading-svgs/speech-bub-big.svg' alt='speech-bubble-big' className='speech-bubble-big'></img>
                 <h3 className='banner-text'>WE ARE WEBCOMIC UNSW</h3>
             </div>
+            
+            <img src='src/WCHome/heading-svgs/comic-panel2.svg' alt='comic-panel-2' className='comic-panel-2'></img>
+            <img src='src/WCHome/heading-svgs/speech-bub-small.svg' alt='speech-bubble-small' className='speech-bubble-small'></img>
         </div>
         
-        <img src='src/WCHome/heading-svgs/comic-panel2.svg' alt='comic-panel-2'></img>
+        
     </div>
   )
 }
