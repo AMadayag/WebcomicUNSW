8964af444249ee11d3644bbbd640d74c653c84cf
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-06-08 20:35:36 +1000

    Fixed sizing issue for homepage

--- src/WCHome/Heading.jsx
+++ src/WCHome/Heading.jsx
@@ -4,14 +4,14 @@ function Heading() {
   return (
     <div className='heading'>
         <div className='panel'>
-            <img src='public/comic-panel1.svg' alt='comic-panel-1' className='comic-panel-1'></img>
+            <img src='comic-panel1.svg' alt='comic-panel-1' className='comic-panel-1'></img>
             <div className='banner'>
-                <img src='public/speech-bub-big.svg' alt='speech-bubble-big' className='speech-bubble-big'></img>
+                <img src='speech-bub-big.svg' alt='speech-bubble-big' className='speech-bubble-big'></img>
                 <h3 className='banner-text'>WE ARE WEBCOMIC UNSW</h3>
             </div>
             
-            <img src='public/heading-svgs/comic-panel2.svg' alt='comic-panel-2' className='comic-panel-2'></img>
-            <img src='public/speech-bub-small.svg' alt='speech-bubble-small' className='speech-bubble-small'></img>
+            <img src='comic-panel2.svg' alt='comic-panel-2' className='comic-panel-2'></img>
+            <img src='speech-bub-small.svg' alt='speech-bubble-small' className='speech-bubble-small'></img>
         </div>
     </div>
   )
